78eb8481f710d52afe9a9a124a544f478d59a6db
fuzongfei <zongfei.fu@yunzhanghu.com>
2020-08-13 22:51:40 +0800

    202008132250

--- yasql-fe/src/views/sqlorders/route.js
+++ yasql-fe/src/views/sqlorders/route.js
@@ -1,38 +1,38 @@
 const route = {
   name: 'view.sqlorders',
-  path: 'sqlorders',
+  path: '/sqlorders',
   component: () => import('./index.vue'),
   redirect: {name: 'view.sqlorders.list'},
   meta: { title: 'DB工单', keepAlive: true, icon: 'database' },
   children: [
     {
       name: 'view.sqlorders.list',
-      path: 'list',
+      path: '/sqlorders/list',
       component: () => import('./list.vue'),
       meta: { title: '工单列表', keepAlive: true, icon: 'bars' }
     },
     {
       name: 'view.sqlorders.detail',
-      path: 'detail/:order_id',
+      path: '/sqlorders/detail/:order_id',
       hidden: true,
       component: () => import('./detail.vue'),
       meta: { title: '工单详情', keepAlive: true }
     },
     {
       name: 'view.sqlorders.commit.ddl',
-      path: 'commit/ddl',
+      path: '/sqlorders/commit/ddl',
       component: () => import('./commit.vue'),
       meta: { title: '提交DDL工单', keepAlive: true, icon: 'bars' }
     },
     {
       name: 'view.sqlorders.commit.dml',
-      path: 'commit/dml',
+      path: '/sqlorders/commit/dml',
       component: () => import('./commit.vue'),
       meta: { title: '提交DML工单', keepAlive: true, icon: 'bars' }
     },
     {
       name: 'view.sqlorders.commit.export',
-      path: 'commit/export',
+      path: '/sqlorders/commit/export',
       component: () => import('./commit.vue'),
       meta: { title: '提交导出工单', keepAlive: true, icon: 'bars' }
     },
